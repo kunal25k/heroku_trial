11f54af4c27314d04462e0d50b685f5a73e6ce0a
Kunal Kulkarni <67704124+kunal25k@users.noreply.github.com>
2022-07-13 13:37:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template, request
 # import marks as m
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import (LinearRegression, Ridge, Lasso)
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+import pickle 
+from pickle import dump, load
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 app = Flask(__name__)
 
